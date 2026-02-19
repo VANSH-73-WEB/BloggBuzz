c67ea2e4baff78a7b933045fe32512ba6d3852b1
vansh parashar <v333777abc@gmail.com>
2026-02-19 12:23:57 +0530

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,4 @@
-// const { validationToken } = require("../services/authentication");
 
-// function checkForAuthenticationCookie(cookieName){
-//   return (req , res , next) =>{
-//     const tokencookievalue = req.cookies[cookieName];
-//     if(!tokencookievalue){
-//      return next();
-//     }
-//     try{
-
-//       const userPayload = validationToken(tokencookievalue);
-//      req.user = userPayload;
-     
-//     }
-//     catch(error){}
-//     return next();
-//   };
-// }
-
-// module.exports = {
-//   checkForAuthenticationCookie,
-// };
 const { validationToken } = require("../services/authentication");
 const User = require("../modals/user");
 
